0c7a31ff1979aae96c4963d01af2e83481ae3141
jasongoose <ssjk7789@gmail.com>
2025-03-08 22:49:09 +0900

    GlobalStyle을 styled-ui로 분리

--- src/components/styled-ui.tsx
+++ src/components/styled-ui.tsx
@@ -1,4 +1,85 @@
-import styled from "styled-components";
+import styled, {createGlobalStyle} from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  /* http://meyerweb.com/eric/tools/css/reset/
+   v5.0.1 | 20191019
+   License: none (public domain)
+*/
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, menu, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  main, menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, main, menu, nav, section {
+    display: block;
+  }
+
+  /* HTML5 hidden-attribute fix for newer browsers */
+  *[hidden] {
+    display: none;
+  }
+
+  body {
+    line-height: 1;
+  }
+
+  menu, ol, ul {
+    list-style: none;
+  }
+
+  blockquote, q {
+    quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Big Shoulders", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 200;
+    font-style: normal;
+    background-color: ${(props) => props.theme.bgColor};
+    color: ${(props) => props.theme.textColor};
+    accent-color: ${(props) => props.theme.accentColor};
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
 
 export const Container = styled.div`
   padding: 0 20px;
@@ -43,7 +124,7 @@ export const Loader = styled.h1`
   text-align: center;
 `;
 
-export const Img = styled.img`
+export const Img = styled.img.attrs({loading: 'lazy'})`
   width: 35px;
   height: 35px;
 `
