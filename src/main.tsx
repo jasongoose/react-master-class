8f6e64788a46bb037e4f22ddaa7aef3cd26f6b81
jasongoose <ssjk7789@gmail.com>
2025-03-08 22:48:20 +0900

    ReactQueryDevtools 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,21 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import App from './App.tsx'
 import {ThemeProvider} from "styled-components";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+
+import App from './App.tsx'
 import {theme} from "./theme.ts";
 
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <ThemeProvider theme={theme}>
-        <App/>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <App/>
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={true}/>
+      </QueryClientProvider>
     </StrictMode>,
 )
