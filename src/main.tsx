b2d6b8f43703724e62f6ebc52f79df44fddc1e01
jasongoose <ssjk7789@gmail.com>
2025-03-14 00:58:33 +0900

    DndProvider 지정

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import {createRoot} from 'react-dom/client'
 import {ThemeProvider} from "styled-components";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+import {HTML5Backend} from 'react-dnd-html5-backend'
+import {DndProvider} from 'react-dnd'
 
 import App from './App.tsx'
 import {theme} from "./theme.ts";
@@ -13,7 +15,9 @@ createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <ThemeProvider theme={theme}>
-          <App/>
+          <DndProvider backend={HTML5Backend}>
+            <App/>
+          </DndProvider>
         </ThemeProvider>
         <ReactQueryDevtools initialIsOpen={true}/>
       </QueryClientProvider>
