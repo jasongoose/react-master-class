70d4036dc811106f65d3a435361d9178214170da
jasongoose <ssjk7789@gmail.com>
2025-03-16 14:03:15 +0900

    Animation part done

--- src/routes/Motion.tsx
+++ src/routes/Motion.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {motion} from "motion/react"
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,18 +10,18 @@ const Wrapper = styled.div`
   background: linear-gradient(135deg, #e09, #d0e);
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function Motion() {
   return (
       <Wrapper>
-        <Box/>
+        <Box initial={{scale: 0}} animate={{scale: 1, rotateZ: 360}} transition={{type: "spring", damping: 100}}/>
       </Wrapper>
   )
 }
