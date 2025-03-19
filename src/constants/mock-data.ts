682ee50777c9e8e587b63836a61e828929159c26
jasongoose <ssjk7789@gmail.com>
2025-03-20 00:41:03 +0900

    mock data 정의

---
+++ src/constants/mock-data.ts
@@ -0,0 +1,103 @@
+export type Book = {
+  id: string;
+  name: string;
+  chapters: string[];
+  characters: string[];
+}
+
+export type Author = {
+  id: string;
+  name: string;
+  books: Book[];
+}
+
+export type MockData = Author[];
+
+export const mockData: MockData = [
+  {
+    id: 'J._K._Rowling',
+    name: 'J. K. Rowling',
+    books: [
+      {
+        id: "Harry_Potter_I",
+        name: "Harry Potter I",
+        chapters: [
+          'Chapter I',
+          'Chapter II',
+          'Chapter III',
+          'Chapter IV',
+          'Chapter V'
+        ],
+        characters: [
+          'Good Guy A',
+          'Good Guy B',
+          'Bad Guy A',
+          'Bad Guy B',
+        ],
+      },
+      {
+        id: "Harry_Potter_II",
+        name: "Harry Potter II",
+        chapters: [
+          'Chapter I',
+          'Chapter II',
+          'Chapter III',
+          'Chapter IV',
+          'Chapter V'
+        ],
+        characters: [
+          'Good Guy A',
+          'Good Guy B',
+          'Bad Guy A',
+          'Bad Guy B',
+        ],
+      },
+      {
+        id: "Harry_Potter_III",
+        name: "Harry Potter III",
+        chapters: [],
+        characters: [],
+      }
+    ],
+  },
+  {
+    id: 'J._R._R._Tolkein',
+    name: 'J. R. R. Tolkein',
+    books: [
+      {
+        id: "Lord_of_the_Rings_I",
+        name: "Lord of the Rings I",
+        chapters: [
+          'Chapter I',
+          'Chapter II',
+          'Chapter III',
+          'Chapter IV',
+          'Chapter V'
+        ],
+        characters: [
+          'Good Guy A',
+          'Good Guy B',
+          'Bad Guy A',
+          'Bad Guy B',
+        ],
+      },
+      {
+        id: "Lord_of_the_Rings_II",
+        name: "Lord of the Rings II",
+        chapters: [
+          'Chapter I',
+          'Chapter II',
+          'Chapter III',
+          'Chapter IV',
+          'Chapter V'
+        ],
+        characters: [
+          'Good Guy A',
+          'Good Guy B',
+          'Bad Guy A',
+          'Bad Guy B',
+        ],
+      }
+    ],
+  }
+]
