e69cac9f66a4d5de27af761981c27dd31c616597
jasongoose <ssjk7789@gmail.com>
2025-03-20 01:14:05 +0900

    Chapters, Characters page 완성

--- src/pages/Chapters.tsx
+++ src/pages/Chapters.tsx
@@ -1,5 +1,30 @@
+import {useParams} from "react-router";
+import {mockData} from "../constants/mock-data.ts";
+
+type Params = {
+  authorId: string;
+  bookId: string;
+}
+
 function Chapters() {
-  return null;
+  const {authorId = '', bookId = ''} = useParams<Params>();
+  const targetAuthor = mockData.find(el => el['id'] === authorId);
+
+  if (!targetAuthor) {
+    return null;
+  }
+
+  const targetBook = targetAuthor['books'].find(book => book['id'] === bookId);
+
+  if (!targetBook) {
+    return null;
+  }
+
+  return (
+      <ul>
+        {targetBook['chapters'].map((chapter) => (<li key={chapter}>{chapter}</li>))}
+      </ul>
+  )
 }
 
 export default Chapters;
--- src/pages/Characters.tsx
+++ src/pages/Characters.tsx
@@ -1,5 +1,30 @@
+import {useParams} from "react-router";
+import {mockData} from "../constants/mock-data.ts";
+
+type Params = {
+  authorId: string;
+  bookId: string;
+}
+
 function Characters() {
-  return null;
+  const {authorId = '', bookId = ''} = useParams<Params>();
+  const targetAuthor = mockData.find(el => el['id'] === authorId);
+
+  if (!targetAuthor) {
+    return null;
+  }
+
+  const targetBook = targetAuthor['books'].find(book => book['id'] === bookId);
+
+  if (!targetBook) {
+    return null;
+  }
+
+  return (
+      <ul>
+        {targetBook['characters'].map((character) => (<li key={character}>{character}</li>))}
+      </ul>
+  )
 }
 
 export default Characters;
