cd33fa34b6c5dce775c1d54c14d264a04fa1f330
jasongoose <ssjk7789@gmail.com>
2025-03-20 22:54:18 +0900

    GlobalStyle 정의 + font 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import {BrowserRouter, Route, Routes} from "react-router";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
+import GlobalStyle from "./GlobalStyle.tsx";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/character/:id" element={<Detail/>}></Route>
-        </Routes>
-      </BrowserRouter>
+      <>
+        <GlobalStyle/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/character/:id" element={<Detail/>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </>
   )
 }
 
