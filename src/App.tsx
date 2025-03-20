d70c6d50532336c8d7c539afab70da3be053eabb
jasongoose <ssjk7789@gmail.com>
2025-03-20 22:23:26 +0900

    Home, Detail route 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import {BrowserRouter, Route, Routes} from "react-router";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
+
 function App() {
-
-
   return (
-      <></>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/character/:id" element={<Detail/>}></Route>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
