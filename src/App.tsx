a4b1df45c307dec8cf0fb903e64de7b57f8bf49c
jasongoose <ssjk7789@gmail.com>
2025-03-23 00:31:56 +0900

    ThemeSwitch 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import {BrowserRouter, Route, Routes} from "react-router";
+import styled from "styled-components";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import GlobalStyle from "./utils/GlobalStyle.tsx";
+import ThemeSwitch from "./ui/parts/ThemeSwitch.tsx";
+
+const ThemeSwitchWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`;
 
 function App() {
   return (
@@ -13,6 +21,9 @@ function App() {
             <Route path="character/:id" element={<Detail/>}/>
           </Routes>
         </BrowserRouter>
+        <ThemeSwitchWrapper>
+          <ThemeSwitch size={30}/>
+        </ThemeSwitchWrapper>
       </>
   )
 }
---
+++ src/ui/parts/ThemeSwitch.tsx
@@ -0,0 +1,34 @@
+import {useAtom} from "jotai";
+import styled from "styled-components";
+import {themeIndexAtom} from "../../utils/atoms.ts";
+import IconSunOutlined from "./icon/IconSunOutlined.tsx";
+import IconMoonFilled from "./icon/IconMoonFilled.tsx";
+
+type Props = {
+  size: number;
+}
+
+const ThemeSwitchContainer = styled.div<{ $size: number }>`
+  width: ${(props) => props.$size + 'px'};
+  height: ${(props) => props.$size + 'px'};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`
+
+function ThemeSwitch(props: Props) {
+  const [themeIndex, setThemeIndex] = useAtom(themeIndexAtom);
+
+  const handleContainerClick = () => {
+    setThemeIndex(1 - themeIndex);
+  }
+
+  return (
+      <ThemeSwitchContainer $size={props.size + 30} onClick={handleContainerClick}>
+        {themeIndex ? <IconSunOutlined size={props.size}/> : <IconMoonFilled size={props.size}/>}
+      </ThemeSwitchContainer>
+  )
+}
+
+export default ThemeSwitch;
