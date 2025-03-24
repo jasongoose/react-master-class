eeeab09206765ba7fa4ecd5c38ca20b1e3a417cf
jasongoose <ssjk7789@gmail.com>
2025-03-24 23:31:46 +0900

    App.module.css 정의 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+import style from "./App.module.css"
+
 function App() {
-
-
   return (
-      <></>
+      <main>
+        <fieldset>
+          <legend>
+            <h1>내가 가고 싶은 나라들</h1>
+          </legend>
+          <form className={style['form-layout']}>
+            <input type="text" placeholder="이름"/>
+            <button>가자!</button>
+          </form>
+        </fieldset>
+        <fieldset>
+          <legend>
+            <h1>내가 가본 나라들</h1>
+          </legend>
+          <ul>
+            <li></li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <legend><h1>내가 좋아하는 나라들</h1></legend>
+          <ul>
+            <li></li>
+          </ul>
+        </fieldset>
+      </main>
   )
 }
 
