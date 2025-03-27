ef1e089520c19bc2d97106697c528289ae6215ab
jasongoose <ssjk7789@gmail.com>
2025-03-27 20:59:58 +0900

    timerControlAtom 정의

--- src/atoms/timer.ts
+++ src/atoms/timer.ts
@@ -1,39 +1,41 @@
 import {atomWithReducer} from "jotai/utils";
 
-type Timer = {
-  time: number;
-  isRunning: boolean;
-}
-
-enum ActionType {
+enum Action {
   START,
   PAUSE,
   RESET,
   TICK,
 }
 
+type Timer = {
+  time: number;
+  isRunning: boolean;
+}
+
+type ActionType = { type: Action };
+
 const MINUTES_TO_SECONDS = 60;
 
 const TWENTY_FIVE_MINUTES_TO_SECONDS = 25 * MINUTES_TO_SECONDS;
 
-const initialTimer: Timer = {
+const generateInitialTimer = (): Timer => ({
   time: TWENTY_FIVE_MINUTES_TO_SECONDS,
   isRunning: false,
-}
+})
 
 const timerControlReducer = (curr: Timer, action?: ActionType) => {
-  switch (action) {
-    case ActionType.START:
-      return curr; // 임시지정
-    case ActionType.PAUSE:
-      return curr; // 임시지정
-    case ActionType.RESET:
-      return curr; // 임시지정
-    case ActionType.TICK:
-      return curr; // 임시지정
+  switch (action?.['type']) {
+    case Action.START:
+      return ({...curr, isRunning: true});
+    case Action.PAUSE:
+      return ({...curr, isRunning: false});
+    case Action.RESET:
+      return generateInitialTimer();
+    case Action.TICK:
+      return ({...curr, time: Math.max(curr['time'] - 1, 0)});
     default:
       return curr;
   }
 };
 
-export const timerControlAtom = atomWithReducer<Timer, ActionType>(initialTimer, timerControlReducer);
+export const timerControlAtom = atomWithReducer<Timer, ActionType>(generateInitialTimer(), timerControlReducer);
