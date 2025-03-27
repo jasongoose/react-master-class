faf9dd4d977465844a7daa7bdd1c07cfcd481489
jasongoose <ssjk7789@gmail.com>
2025-03-27 21:31:24 +0900

    분, 잔여 초를 계산하는 math util 생성

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,6 +1,15 @@
 export const MINUTES_TO_SECONDS = 60;
-export const getDecimalDigits = (num: number) => {
-  const firstDigit = num / 10;
+
+export const calcMinutesFromSeconds = (totalSeconds: number) => {
+  return Math.floor(totalSeconds / MINUTES_TO_SECONDS);
+}
+
+export const calcSecondsLeft = (totalSeconds: number) => {
+  return totalSeconds % MINUTES_TO_SECONDS;
+}
+
+export const calcDecimalDigits = (num: number) => {
+  const firstDigit = Math.floor(num / 10);
   const secondDigit = num % 10;
   return [firstDigit, secondDigit];
 }
