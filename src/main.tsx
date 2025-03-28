2188ba313809f3edad970ebb4e58e47e7e366cda
jasongoose <ssjk7789@gmail.com>
2025-03-29 01:39:34 +0900

    ThemeSwitch, GlobalStyle 모두 Providers로 이동

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
-import GlobalStyle from "./utils/GlobalStyle.tsx";
 import Providers from "./utils/Providers.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <GlobalStyle/>
       <Providers>
         <App/>
       </Providers>
---
+++ src/ui/pieces/ThemeSwitchWrapper.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ThemeSwitchWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+`;
