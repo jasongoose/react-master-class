df9aca1cfdb2553c51f7f67e2f7570abc9feb12c
jasongoose <ssjk7789@gmail.com>
2025-03-29 01:11:28 +0900

    react-query Provider 세팅

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,8 +1,12 @@
 import styled, {ThemeProvider} from "styled-components";
 import type {PropsWithChildren} from "react";
 import {useAtomValue} from "jotai/react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 import {themeAtom} from "../atoms/theme.ts";
 import ThemeSwitch from "../ui/parts/ThemeSwitch.tsx";
+
+const queryClient = new QueryClient();
 
 const ThemeSwitchWrapper = styled.div`
   position: fixed;
@@ -15,7 +19,10 @@ function Providers({children}: PropsWithChildren) {
 
   return (
       <ThemeProvider theme={theme}>
-        {children}
+        <QueryClientProvider client={queryClient}>
+          {children}
+          <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+        </QueryClientProvider>
         <ThemeSwitchWrapper>
           <ThemeSwitch size={30}/>
         </ThemeSwitchWrapper>
