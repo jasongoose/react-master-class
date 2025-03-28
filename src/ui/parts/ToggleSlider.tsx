1f9311f70ab67eb84a04e028a088a4a902e5fad5
jasongoose <ssjk7789@gmail.com>
2025-03-29 00:08:46 +0900

    Start, Pause를 위한 ToggleSlider 버튼 정의

---
+++ src/ui/parts/ToggleSlider.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import {motion} from "motion/react";
+
+type Props = {
+  isOn: boolean;
+  onText: string;
+  offText: string;
+  handleToggleChipClick: () => void;
+}
+
+const ToggleContainer = styled.div<{ $isOn: boolean }>`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: ${(props) => props.$isOn ? "flex-start" : "flex-end"};
+  outline: 1px solid black;
+  background-color: black;
+`;
+
+const ToggleChip = styled(motion.div)`
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
+  border-collapse: collapse;
+  cursor: pointer;
+`;
+
+const ToggleChipText = styled.span`
+  font-size: 30px;
+`;
+
+function ToggleSlider({isOn, onText, offText, handleToggleChipClick}: Props) {
+  return (
+      <ToggleContainer $isOn={isOn}>
+        <ToggleChip layout onClick={handleToggleChipClick}>
+          <ToggleChipText>
+            {isOn ? offText : onText}
+          </ToggleChipText>
+        </ToggleChip>
+      </ToggleContainer>
+  )
+}
+
+export default ToggleSlider;
