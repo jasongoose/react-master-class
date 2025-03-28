4b92ea8ff2a4799ef4b5d1831de5cbe86064fe34
jasongoose <ssjk7789@gmail.com>
2025-03-29 00:10:03 +0900

    Round, Goal 표시를 위한 Battery 컴포넌트 생성

---
+++ src/ui/pieces/Battery.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+type Props = {
+  currentGauge: number;
+  totalGauge: number;
+}
+
+const BatteryContainer = styled.div<{ $totalGauge: number; }>`
+  display: grid;
+  grid-template-columns: ${(props) => `repeat(${props.$totalGauge}, 1fr)`};
+  gap: 5px;
+  padding: 5px;
+  outline: 1px solid black;
+`;
+
+const BatterySegment = styled.div<{ $isFilled: boolean }>`
+  height: 30px;
+  background-color: ${(props) => props.$isFilled ? 'green' : 'transparent'};
+  outline: 1px dashed ${(props) => props.$isFilled ? 'none' : 'green'};
+`;
+
+function Battery({currentGauge, totalGauge}: Props) {
+  const gaugeArr = Array.from(Array(totalGauge), (_, i) => i + 1);
+
+  return (
+      <BatteryContainer $totalGauge={totalGauge}>
+        {gaugeArr.map((gaugeNumber) => (
+            <BatterySegment $isFilled={gaugeNumber <= currentGauge}/>
+        ))}
+      </BatteryContainer>
+  )
+}
+
+export default Battery;
