5601abfa81002b02f6b3b8f01432f1aec4ea467f
jasongoose <ssjk7789@gmail.com>
2025-03-31 07:44:28 +0900

    api 호출 함수들을 util 폴더에 정리

---
+++ src/utils/api.ts
@@ -0,0 +1,27 @@
+const BASE_URL = 'https://movies-api.nomadcoders.workers.dev';
+
+export function getPopular() {
+  return fetch(`${BASE_URL}/popular`).then((r) => r.json());
+}
+
+export function getNowPlaying() {
+  return fetch(`${BASE_URL}/now-playing`).then((r) => r.json());
+}
+
+export function getComingSoon() {
+  return fetch(`${BASE_URL}/coming-soon`).then((r) => r.json());
+}
+
+export function getMovie(id: string | number) {
+  return fetch(`${BASE_URL}/movie?id=${id}`).then((r) => r.json());
+}
+
+// https://developer.themoviedb.org/docs/image-basics
+
+export function makeImagePath(imageUrl: string) {
+  return `https://image.tmdb.org/t/p/w500${imageUrl}`;
+}
+
+export function makeBgPath(imageUrl: string) {
+  return `https://image.tmdb.org/t/p/original${imageUrl}`;
+}
