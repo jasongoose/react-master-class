f8d502786973d5203461923c2d7b82c2f1d230be
jasongoose <ssjk7789@gmail.com>
2025-03-31 21:04:16 +0900

    router 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router';
+import Home from './routes/Home.tsx';
+import ComingSoon from './routes/ComingSoon.tsx';
+import NowPlaying from './routes/NowPlaying.tsx';
+
 function App() {
-  return <>{/* FROMHERE:: */}</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/comming-soon" element={<ComingSoon />} />
+        <Route path="/now-playing" element={<NowPlaying />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
