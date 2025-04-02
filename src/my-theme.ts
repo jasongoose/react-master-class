18de385c6e34fc23cc04d378b1a4a19d749aa5f4
jasongoose <ssjk7789@gmail.com>
2025-04-03 01:56:50 +0900

    darkTheme, lightTheme에 mainBlur, contrastBlur 추가

--- src/my-theme.ts
+++ src/my-theme.ts
@@ -2,10 +2,14 @@ import type { DefaultTheme } from 'styled-components';
 
 export const darkTheme: DefaultTheme = {
   main: '#111',
-  contrast: 'whiteSmoke'
+  contrast: 'whiteSmoke',
+  mainBlur: 'rgba(17,17,17,0.8)',
+  contrastBlur: 'rgba(245,245,245,0.8)'
 };
 
 export const lightTheme: DefaultTheme = {
   main: 'whiteSmoke',
-  contrast: '#111'
+  contrast: '#111',
+  mainBlur: 'rgba(245,245,245,0.8)',
+  contrastBlur: 'rgba(17,17,17,0.8)'
 };
